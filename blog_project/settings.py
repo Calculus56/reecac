cedb7ea1e6379a0b57c6ba7969a915c408c5a8b9
Calculus56 <ramostim7416@gmail.com>
2020-07-19 20:44:38 -0400

    deleted django_locals

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -150,5 +150,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
