4215e2b6d226a6ebdc4a86aa16bb0ef9758e7d75
Calculus56 <ramostim7416@gmail.com>
2020-07-20 02:27:24 -0400

    Added more sitemaps

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -22,3 +22,36 @@ class StaticViewContact(Sitemap):
 
     def location(self, item):
         return reverse(item)
+
+
+class StaticViewPolicy(Sitemap):
+
+    priority = 0.6
+
+    def items(self):
+        return ['policy']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class StaticViewTeam(Sitemap):
+
+    priority = 0.6
+
+    def items(self):
+        return ['team']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class StaticViewAbout(Sitemap):
+
+    priority = 0.6
+
+    def items(self):
+        return ['about']
+
+    def location(self, item):
+        return reverse(item)
